c5b110d0bbfa529c6ec907e91a790935657f8ec8
James Garbutt <43081j@users.noreply.github.com>
2019-12-27 15:57:59 +0000

    add eslint

--- src/Visitor.ts
+++ src/Visitor.ts
@@ -4,22 +4,41 @@ import {
   PackageLockError
 } from './PackageLock';
 
+/**
+ * Base implementation of a package-lock visitor.
+ */
 export abstract class Visitor {
   public errors: Set<PackageLockError> = new Set();
 
+  /**
+   * Visits a given package lock object
+   * @param root Package lock data to analyze
+   */
   public abstract visit(root: PackageLock): Promise<void>;
+
+  /**
+   * Visits a single dependency of a package
+   * @param name Dependency name being analyzed
+   * @param data Dependency being analyzed
+   */
   public abstract visitDependency(
     name: string,
     data: PackageLockDependency
   ): Promise<void>;
 
-  protected async visitDependencies(
+  /**
+   * Visits each dependency in a given set of dependencies
+   * @param deps Dictionary-like collection of dependencies to analyze
+   */
+  protected async _visitDependencies(
     deps: Record<string, PackageLockDependency>
   ): Promise<void> {
     const results: Promise<void>[] = [];
 
     for (const dep in deps) {
-      results.push(this.visitDependency(dep, deps[dep]));
+      if (Object.prototype.hasOwnProperty.call(deps, dep)) {
+        results.push(this.visitDependency(dep, deps[dep]));
+      }
     }
 
     await Promise.all(results);
--- src/visitors/InsecureUriVisitor.ts
+++ src/visitors/InsecureUriVisitor.ts
@@ -1,19 +1,24 @@
 import {Visitor} from '../Visitor';
 import {PackageLock, PackageLockDependency} from '../PackageLock';
 
+/**
+ * Detects insecure URLs in dependencies (i.e. non-https)
+ */
 export class InsecureUriVisitor extends Visitor {
+  /** @inheritdoc */
   public async visit(data: PackageLock): Promise<void> {
     if (data.dependencies) {
-      await this.visitDependencies(data.dependencies);
+      await this._visitDependencies(data.dependencies);
     }
   }
 
+  /** @inheritdoc */
   public async visitDependency(
     name: string,
     data: PackageLockDependency
   ): Promise<void> {
     if (data.dependencies) {
-      await this.visitDependencies(data.dependencies);
+      await this._visitDependencies(data.dependencies);
     }
 
     if (data.resolved && data.resolved.startsWith('http://')) {
